bbd7f831fca82a825578f8a8b4bd07852930417a
robert <toanv97.vn@gmail.com>
2026-04-30 01:09:37 +0700

    [DEV] Remove swap files

---
+++ blink/clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Configuration
+PROJECT="blink_v2"
+PORT="/dev/ttyACM0"
+PORT="/dev/ttyUSB0"
+BAUD="19200"
+CHIP="89s52"
+CONF_8051="/home/robert/Projects/AT89S52-asm-isp/89s52.conf"
+
+# 1. Clean previous builds
+echo "--- Cleaning up ---"
+rm -f ${PROJECT}.ihx ${PROJECT}.rel ${PROJECT}.lst ${PROJECT}.sym ${PROJECT}.map ${PROJECT}.mem
+
